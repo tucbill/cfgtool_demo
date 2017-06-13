3082333a8049b88893d1d62628a6e40ed3f88967
Bill Owen <billowen@us.ibm.com>
2017-06-12 17:23:14 -0700

    Small changes

--- src/cfgcompare.py
+++ src/cfgcompare.py
@@ -1,24 +1,23 @@
 import json
+import os
 
 PROFILE_PATH = '/usr/lpp/mmfs/profiles'
-GIT_ROOT = '/Users/billowen/git/config-monkey'
+GIT_ROOT = '~/git/config-monkey'
 NCMAP_PATH = 'specs/spectrum-scale/mmconfig-profiles/node_class_map.json'
 
 
 def invert_ncmap(ncmap):
     profile_map = {}
-    for d in ncmap['specs']:
-        for nc in d['node_classes']:
-            profile_map[nc] = d['name']
-    profile_map = {nc: d['name'] for d in ncmap['specs'] 
+    profile_map = {nc: d['profile_name'] for d in ncmap['specs'] 
                    for nc in d['node_classes']}
     return profile_map
 
 
 def main():
-    nodeclass = 'fponodes'
+    nodeclass = 'fpo_nodes'
 
-    with open('%s/%s' % (GIT_ROOT, NCMAP_PATH), 'r') as f:
+    nodeclass_full_path = os.path.expanduser('%s/%s' % (GIT_ROOT, NCMAP_PATH))
+    with open(nodeclass_full_path, 'r') as f:
         ncmap = json.load(f)
 
     print ncmap
